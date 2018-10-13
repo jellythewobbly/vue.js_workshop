3f5bf5dcce9e105784e89526642c99d19cf5ea07
jellythewobbly <jellythewobbly@hotmail.com>
2018-10-13 19:01:52 +0800

    initial

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+		<my-counter v-for="item in items" :key="item.label"
+			:rate="item.price"
+			:available="available"
+			:fruit="item.label"
+			@update="updateAvailable"></my-counter>
+  </div>
+</template>
+
+<script>
+import MyCounter from './components/MyCounter.vue';
+
+export default {
+	name: 'app',
+	components: {
+		MyCounter,
+	},
+	data() {
+		return {
+			available: 100,
+			count: 0,
+			items: [
+				{ label: 'Apples', price: 5 },
+				{ label: 'Bananas', price: 10 },
+				{ label: 'Coconuts', price: 20 },
+			],
+		};
+	},
+	methods: {
+		updateAvailable(value) {
+			this.available -= value;
+		},
+		fetchItems() {
+			setTimeout(() => {
+				this.items = [
+					{ label: 'Apples', price: 5 },
+					{ label: 'Bananas', price: 10 },
+					{ label: 'Coconuts', price: 20 },
+				];
+			}, 1000);
+		},
+	},
+	created() {
+		window.vm = this;
+		this.fetchItems();
+	},
+	mounted() {},
+};
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
---
+++ src/components/MyCounter.vue
@@ -0,0 +1,49 @@
+<template>
+	<div>
+		<div class="counter">
+			<div>Number of {{fruit}}: {{count}}</div>
+			<div>Please pay $ {{price}}</div>
+			<button @click="increment()">Click</button>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	props: ['available', 'rate', 'fruit'],
+	data() {
+		return {
+			toggle: true,
+			count: 0,
+		};
+	},
+	computed: {
+		price() {
+			return this.rate * this.count;
+		},
+		countDisplay() {
+			return 'My count is: ' + this.count;
+		},
+	},
+	methods: {
+		increment() {
+			if (this.available < this.rate) return;
+			this.count++;
+			this.$emit('update', this.rate);
+		},
+	},
+};
+</script>
+
+<style>
+.counter {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 20px;
+}
+</style>
+
+
